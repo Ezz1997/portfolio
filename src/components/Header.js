3a8536b5dd6b79088529ee902f0bc9fb71f55488
Ezz Maree <ezmarie97@gmail.com>
2023-12-13 18:29:28 +0200

    made the header responsive for mobile devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -75,7 +75,7 @@ const Header = () => {
     >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
-          px={16}
+          px={[6, 16]}
           py={4}
           justifyContent="space-between"
           alignItems="center"
